a9aa550884093fe383da7403966b7472befa640b
raedboumm <raedboumm@gmail.com>
2026-06-12 22:01:30 +0100

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,10 @@
+function getClothingCategory(category) {
+    switch (category) {
+        case "Formal":
+            return "Wear a suit or dress.";
+        case "Casual":
+            return "Wear jeans and a t-shirt.";
+        case "Sport":
+            return "Wear athletic wear and sneakers.";
+        default:
+            return "Wear whatever makes you comfortable!";
